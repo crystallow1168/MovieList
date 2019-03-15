a070edc0b5ab5b761be32366d772d2fb6ff8dcd1
Crystal.L <crystal.l0815@gmail.com>
2019-03-15 16:30:55 -0700

    Implement bootstrap button

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -18,7 +18,7 @@ class AddMovie extends React.Component {
         return (
             <div className="add-bar">
             <input type="text" placeholder="Add Movie..." value={this.state.myList} onChange={ (e) => this.handleChange(e) } ></input>
-            <button onClick={ () => this.props.submit(this.state.myList)}>Submit</button>
+            <button className="btn btn-primary" onClick={ () => this.props.submit(this.state.myList)}>Submit</button>
             </div>
         )
         
