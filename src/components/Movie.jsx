dd332e4b35e18acce67fc19be112706858fc1c13
Crystal.L <crystal.l0815@gmail.com>
2019-03-15 16:31:57 -0700

    Implement toggle badge on movie

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,10 +1,13 @@
 const Movie = (props) => (
     <li className="list-group-item d-flex justify-content-between align-items-center">
         {props.movie.title}
-        <span className="badge badge-primary badge-pill"></span>
-        <button>Hello</button>
+        <span className="badge badge-primary badge-pill" 
+        
+        onClick={() => this.props.submit(props.movie.title)}>
+        {props.movie.watched ? 'Watched' : 'Not Watched'}
+        </span>
     </li>
-
 )
 
 export default Movie;
+
