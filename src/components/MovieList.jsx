02d1ba2732c34f338fc9f804c3c71912ce601a11
Crystal.L <crystal.l0815@gmail.com>
2019-03-15 16:32:43 -0700

    Implement submit event handler on movie

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,9 +2,15 @@ import Movie from './Movie.js'
 
 const MovieList = (props) => (
    <ul className="list-group">
-      {props.movies.map(movie => <Movie movie={movie} key={movie.title}/>)}
+      {props.movies.map(movie => 
+      <Movie movie={movie} 
+      key={movie.title}
+      
+      submit={props.submit}
+      />)}
    </ul>
 )
 
-export default MovieList
+export default MovieList;
+
 
