ebe4326bd451b5254ddb3e847e67c42e602e7d6a
Crystal.L <crystal.l0815@gmail.com>
2019-03-15 16:44:35 -0700

    Removed 'this' on onClick event handler

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -2,8 +2,8 @@ const Movie = (props) => (
     <li className="list-group-item d-flex justify-content-between align-items-center">
         {props.movie.title}
         <span className="badge badge-primary badge-pill" 
-        
-        onClick={() => this.props.submit(props.movie.title)}>
+
+        onClick={() => props.submit(props.movie.title)}>
         {props.movie.watched ? 'Watched' : 'Not Watched'}
         </span>
     </li>
