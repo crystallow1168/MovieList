613c3dc17964343ec6be12c4d6b8af0408adb97a
Crystal.L <crystal.l0815@gmail.com>
2019-03-15 20:24:10 -0700

    Implement watched and to watch tabs

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,16 +1,31 @@
 import Movie from './Movie.js'
 
 const MovieList = (props) => (
-   <ul className="list-group">
-      {props.movies.map(movie => 
-      <Movie movie={movie} 
-      key={movie.title}
+   
+   <ul className="nav nav-tabs">
+      <li className="nav-item">
+         <a className="nav-link active" href="#">To Watch</a>
+         <ul className="list-group">
+            {props.movies.map(movie => 
+            <Movie movie={movie} 
+            key={movie.title}
+            toggleWatchedBadge={props.toggleWatchedBadge}
+            />)}
+         </ul>
+      </li>
       
-      submit={props.submit}
-      />)}
+      <li>
+      <a className="nav-link active" href="#">Watched</a>
+         <ul className="list-group">
+            {props.movies.map(movie => 
+            <Movie movie={movie} 
+            key={movie.title}
+            toggleWatchedBadge={props.toggleWatchedBadge}
+            />)}
+         </ul>
+      </li>
    </ul>
 )
 
+
 export default MovieList;
-
-
