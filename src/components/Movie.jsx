65de24a2bfd8aff73faf0606c31b1d61cd9a6205
Crystal.L <crystal.l0815@gmail.com>
2019-03-16 21:46:26 -0700

    Implement react component to movie

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,13 +1,35 @@
-const Movie = (props) => (
-    <li className="list-group-item d-flex justify-content-between align-items-center">
-        {props.movie.title}
-        <span className="badge badge-primary badge-pill" 
+class Movie extends React.Component {
+    constructor(props) {
+        super(props)
 
-        onClick={() => props.toggleWatchedBadge(props.movie.title)}>
-        {props.movie.watched ? 'Watched' : 'Not Watched'}
-        </span>
-    </li>
-)
+        this.state = {
+            detailToggle: false
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                <li className="list-group-item d-flex justify-content-between align-items-center"
+                 >
+                    {this.props.movie.title}
+                    <span className="badge badge-primary badge-pill" 
+                    onClick={() => this.props.toggleWatchedBadge(this.props.movie.title)}>
+                    {this.props.movie.watched ? 'Watched' : 'Not Watched'}
+                    </span>
+
+                <div className={this.state.showDetail ? '' : 'd-none'}>
+                    <div>
+                        <div>Release Date</div>
+                        <div>Run Time</div>
+                        <div>Metascore</div>
+                        <div>IMDBRating</div>
+                    </div>
+                </div>
+                </li>
+            </div>
+        )
+    }
+} 
 
 export default Movie;
-
