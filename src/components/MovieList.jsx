8da3ac046240e82ba322279d4c6b06f37a75624d
Crystal.L <crystal.l0815@gmail.com>
2019-03-16 21:45:52 -0700

    Implement component to Movie

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,32 +1,65 @@
 import Movie from './Movie.js'
 
-const MovieList = (props) => (
-   
-   <ul className="nav nav-tabs">
-      <li className="nav-item">
-         <a className="nav-link active" href="#">To Watch</a>
-         <ul className="list-group">
-            {(props.movies.filter(movie => movie.watched === false)).map(movie => 
-            <Movie movie={movie} 
-            key={movie.toWatchTitle}
-            toggleWatchedBadge={props.toggleWatchedBadge}
-            />)}
-         </ul>
-      </li>
-      
-      <li>
-      <a className="nav-link active" href="#">Watched</a>
-      <ul className="list-group">
-            {(props.movies.filter(movie => movie.watched === true)).map(movie => 
-            <Movie movie={movie} 
-            key={movie.toWatchTitle}
-            toggleWatchedBadge={props.toggleWatchedBadge}
-            />)}
-         </ul>
-      </li>
-   </ul>
-)
+class MovieList extends React.Component {
+   constructor(props) {
+      super(props) 
 
+      this.state = {
+         toWatchTabClicked: true,
+         watchedTabClicked: false
+     }
+   }
+
+   handleTabClicked() {
+      console.log('hiii')
+      this.setState ({
+         toWatchTabClicked: !(this.state.toWatchTabClicked),
+         watchedTabClicked: !(this.state.watchedTabClicked)
+      })
+   }
+
+   render() {
+
+      if (this.state.toWatchTabClicked === true) {
+         return (
+            <div>
+               <ul className="nav nav-tabs">
+                  <li className="nav flex-row">
+                     <a className={`nav-link `} onClick={()=> this.handleTabClicked()} href="#">To Watch</a>
+                     <a className={`nav-link active`} onClick={()=> this.handleTabClicked()} href="#">Watched</a>
+                  </li>
+               </ul>
+               <ul className="list-group flex-column">
+                  {(this.props.movies.filter(movie => movie.watched === false)).map(movie => 
+                     <Movie movie={movie} 
+                     key={movie.toWatchTitle}
+                     toggleWatchedBadge={this.props.toggleWatchedBadge}
+                     />)}
+               </ul>
+            </div>
+         )
+
+      } else if (this.state.watchedTabClicked === true)  {
+         
+         return (
+            <div>
+               <ul className="nav nav-tabs">
+                  <li className="nav flex-row">
+                     <a className={`nav-link active`} onClick={()=> this.handleTabClicked()} href="#">To Watch</a>
+                     <a className={`nav-link `} onClick={()=> this.handleTabClicked()} href="#">Watched</a>
+                  </li>
+               </ul>
+               <ul className="list-group">
+                     {(this.props.movies.filter(movie => movie.watched === true)).map(movie => 
+                     <Movie movie={movie} 
+                     key={movie.toWatchTitle}
+                     toggleWatchedBadge={this.props.toggleWatchedBadge}
+                     />)}
+               </ul>
+            </div>
+         )
+      } 
+   }
+}
 
 export default MovieList;
-
